a1c8479c4f7ec4c7f8ced9e05e0c7047e5303100
Elliot Toman <web@asubtleweb.com>
2022-10-01 15:20:41 -0400

    Carousel update

--- wp-content/themes/apollo_asw/snippets/testimonials-carousel.php
+++ wp-content/themes/apollo_asw/snippets/testimonials-carousel.php
@@ -55,7 +55,6 @@ $the_query = new WP_Query( array(
     </div>
     <div class="reviews-links">
         <a class="button gold" href="<?= get_theme_mod('wpurl'); ?>/testimonials">More Reviews</a>
-        <a class="button cyan" target="_blank" href="<?= get_theme_mod('reviews_link'); ?>">Leave a Review</a>
     </div>
  <?php endif; ?>
 
