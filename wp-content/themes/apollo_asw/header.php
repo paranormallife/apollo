7fc391e01e3cc7c6075e7f0ed984d792a4945ab1
Elliot Toman <web@asubtleweb.com>
2022-10-03 04:04:17 -0400

    Add Google tracking code

--- wp-content/themes/apollo_asw/header.php
+++ wp-content/themes/apollo_asw/header.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 
 <head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZFWVGKCG5X"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZFWVGKCG5X');
+</script>
+
 <?php $theme = get_bloginfo('template_directory'); ?>
 <meta http-equiv="Content-Type" content="text/html, charset=UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
